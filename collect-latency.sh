86007dfda49a5c5d30433bcd6f68dc910284df44
TR Murphy <biffsocko@gmail.com>
2023-07-07 09:07:36 -0400

    Update collect-latency.sh

--- collect-latency.sh
+++ collect-latency.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#############################################
+##########################################################################################
 # TR Murphy
 # collect-latency.sh
 #
+# uses: https://pypi.org/project/tcp-latency/
 # put your own IP ADDRESS and PORT in below
 #
 # INSTRUCTIONS:
@@ -11,7 +12,13 @@
 #   python3 -m venv `pwd`
 #   pip3 install tcp-latency
 #   chmod 755 ./collect-latency.sh
-#   nohup ./collect-latency.sh &
+#
+# ADD TO CRON:
+# this will run every minute and will enable the logfile
+# turnover
+#
+# * * * * * /home/unixsa/dev/collect-latency.sh
+#
 #
 # OUTPUT:
 # 12.192.234.186: tcp seq=0 port=22221 timeout=5 time=42.04943589866161 ms
@@ -19,29 +26,34 @@
 # 12.192.234.186: tcp seq=2 port=22221 timeout=5 time=41.26440268009901 ms
 # 12.192.234.186: tcp seq=3 port=22221 timeout=5 time=40.49915075302124 ms
 # 12.192.234.186: tcp seq=4 port=22221 timeout=5 time=40.718947537243366 ms
-#############################################
+#
+# NOTES:
+##########################################################################################
 
-. $HOME/dev/bin/activate
+# source in the python environment to run tcp-latency
+. /home/unixsa/dev/bin/activate
 
+
+IPADDR="12.12.12.12"    # put remote IP here
+PORT="1212"             #  put remote port here
+
+VER=`date +"%H%M"`
+DAY=`date -d "yesterday" +%A`
 LOGFILE="/var/tmp/timelog"
-IPADDY="12.12.12.12"
-PORT="1212"
 
-while [ true ]
-do
-  VER=`date +"%H%M"`
-  DAY=`date +"%A"`
 
-  if [ $VER -eq "0000" ]
+# its a new day - start with a new log file
+if [ $VER -eq "0000" ]
+then
+  if [ -f ${LOGFILE} ]
   then
-    if [ -f ${LOGFILE} ]
-    then
-      mv ${LOGFILE}  ${LOGFILE}.OLD
-    fi
+    # if there is a log file from last week
+    # this will overwrite it - we'll keep
+    # a full week of logs this week without
+    # overgrowning or having to do further cleanup
+    mv -f  ${LOGFILE}  ${LOGFILE}.${DAY}
   fi
+fi
 
-  tcp-latency ${IPADDY} --port ${PORT} | grep 'time='   >> ${LOGFILE}
-  sleep 25
-done
-
+tcp-latency ${IPADDR} --port ${PORT} | grep 'time='   >> ${LOGFILE}
 exit 0
